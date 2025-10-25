0885c95b922e958ca9d02aa2117fc64e0a93b11d
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-10-26 01:26:50 +0200

    add StudentInputDto and StudentItem classes for student data management

---
+++ Zademy.Domain/Students/StudentItem.cs
@@ -0,0 +1,8 @@
+namespace Zademy.Domain.Students;
+
+public class StudentItem
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+}
