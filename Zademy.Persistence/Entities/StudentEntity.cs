41b8db7c0b834b51a65aeaa124ba4252cee2b39d
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-10-26 02:29:37 +0200

    feat: add validation attributes for Name and Email properties in StudentEntity

--- Zademy.Persistence/Entities/StudentEntity.cs
+++ Zademy.Persistence/Entities/StudentEntity.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Zademy.Persistence.Entities;
 
 public class StudentEntity
 {
     public int Id { get; set; }
+
+    [MaxLength(100)]
     public required string Name { get; set; }
+
+    [MaxLength(255)]
     public required string Email { get; set; }
 }
