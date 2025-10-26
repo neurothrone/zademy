55bfac1e1fff7de3a340f49a6dd1ba80d67385e5
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-10-26 02:29:47 +0200

    remove Student.cs

--- Zademy.Api/Models/Student.cs
+++
@@ -1,8 +0,0 @@
-namespace Zademy.Api.Models;
-
-public class Student
-{
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Email { get; set; }
-}
