ad420132b3e358ca85c392f68c98d0f650af16bd
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-10-26 05:21:19 +0100

    feat: populate CourseInstances and Grades in DbContextExtensions

--- Zademy.Persistence/Data/DbContextExtensions.cs
+++ Zademy.Persistence/Data/DbContextExtensions.cs
@@ -23,32 +23,43 @@ public static class DbContextExtensions
         if (!context.Students.Any())
             context.Students.AddRange(student1, student2, student3);
 
-        if (!context.CourseInstances.Any())
+        var courseInstance1 = new CourseInstanceEntity
         {
-            context.CourseInstances.AddRange(
-                new CourseInstanceEntity
+            Id = 1,
+            StartDate = DateTime.UtcNow.AddDays(-10),
+            EndDate = DateTime.UtcNow.AddDays(20),
+            Course = course1,
+            Students = [student1, student2]
+        };
+        var courseInstance2 = new CourseInstanceEntity
+        {
+            Id = 2,
+            StartDate = DateTime.UtcNow.AddDays(-5),
+            EndDate = DateTime.UtcNow.AddDays(25),
+            Course = course2,
+            Students = [student2]
+        };
+        var courseInstance3 = new CourseInstanceEntity
+        {
+            Id = 3,
+            StartDate = DateTime.UtcNow.AddDays(1),
+            EndDate = DateTime.UtcNow.AddDays(30),
+            Course = course2,
+            Students = [student3]
+        };
+
+        if (!context.CourseInstances.Any())
+            context.CourseInstances.AddRange(courseInstance1, courseInstance2, courseInstance3);
+
+        if (!context.Grades.Any())
+        {
+            context.Grades.AddRange(
+                new GradeEntity
                 {
                     Id = 1,
-                    StartDate = DateTime.UtcNow.AddDays(-10),
-                    EndDate = DateTime.UtcNow.AddDays(20),
-                    Course = course1,
-                    Students = [student1, student2]
-                },
-                new CourseInstanceEntity
-                {
-                    Id = 2,
-                    StartDate = DateTime.UtcNow.AddDays(-5),
-                    EndDate = DateTime.UtcNow.AddDays(25),
-                    Course = course2,
-                    Students = [student2]
-                },
-                new CourseInstanceEntity
-                {
-                    Id = 3,
-                    StartDate = DateTime.UtcNow.AddDays(1),
-                    EndDate = DateTime.UtcNow.AddDays(30),
-                    Course = course2,
-                    Students = [student3]
+                    Value = "A",
+                    Student = student1,
+                    CourseInstance = courseInstance1
                 }
             );
         }
