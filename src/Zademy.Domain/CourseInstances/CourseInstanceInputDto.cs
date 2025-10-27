15712a519b6110a23943d411ecf4b4c8354627d7
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-10-27 02:06:39 +0100

    feat: add validation attributes to CourseInstanceInputDto for date and course ID

--- src/Zademy.Domain/CourseInstances/CourseInstanceInputDto.cs
+++ src/Zademy.Domain/CourseInstances/CourseInstanceInputDto.cs
@@ -1,5 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using Zademy.Domain.Validation;
+
 namespace Zademy.Domain.CourseInstances;
 
 public record CourseInstanceInputDto
 {
+    [Required(ErrorMessage = "Start date is required")]
+    [DateString]
+    public string? StartDate { get; init; }
+
+    [Required(ErrorMessage = "End date is required")]
+    [DateString]
+    [DateAfter(comparisonProperty: nameof(StartDate))]
+    public string? EndDate { get; init; }
+
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Course ID must be greater than 0")]
+    public int CourseId { get; init; }
+
+    [Required]
+    [MinLength(1, ErrorMessage = "At least one Student ID is required")]
+    public required List<int> StudentIds { get; init; }
 }
